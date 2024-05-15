4d03d2b8e89cb45f47d5debfc3ba0f455d34c6d9
Ehab Ahmed <ehab.ahmed.yassen@gmail.com>
2024-05-15 20:36:33 +0300

    initial commit

---
+++ hello-world-java/src/main/java/com/example/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.helloWorld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello, World!");
+        System.out.println("This is my first docker image app for Java!");
+    }
+}
